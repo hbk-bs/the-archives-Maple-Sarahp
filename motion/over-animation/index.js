3370f036ca3ba11d00ba8d412d2595458fcf01ae
sarah <s.lutterloh@hbk-bs.de>
2025-03-13 00:28:08 +0100

    röstziebeln direkt aus hand fressen

--- motion/over-animation/index.js
+++ motion/over-animation/index.js
@@ -3,18 +3,18 @@ let y = 0;
 let radius = 50;
 let speed = 1;
 
-let isJumping = false; 
-let jumpProgress = 0;  
+let isJumping = false;
+let jumpProgress = 0;
 
 const x3 = (n) => width * n;
 const y3 = (n) => height * n;
 const s3 = (n) => width * n;
 
-function setup(){
-  const canvas = createCanvas(500,500);
+function setup() {
+  const canvas = createCanvas(500, 500);
   canvas.parent("sketch");
   y = height / 2;
-  x= -radius;
+  x = -radius;
   strokeWeight(0.5);
   fill(0);
 }
@@ -23,7 +23,7 @@ function draw() {
   background(0);
 
   fill(100);
-  rect(x3(0.5),y3(0.5),s3(0.1));
+  rect(x3(0.5), y3(0.5), s3(0.1));
 
   if (x >= 200 && x <= 300) {
     y = height / 2 - 100;
@@ -39,6 +39,6 @@ function draw() {
 
   if (x >= width + radius) {
     x = -radius;
-    speed = 1; 
+    speed = 1;
   }
 }
